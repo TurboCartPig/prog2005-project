7a9634f9dac07cd4a972625f974b549f8c6f0174
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-05-02 18:27:03 +0200

    Move http server into it's own package and gorutine

--- endpoints/endpoints.go
+++ endpoints/endpoints.go
@@ -1,22 +1,61 @@
 package endpoints
 
 import (
-	"github.com/go-chi/chi"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 )
 
-const Version = "V1"
+// Version of the API
+const Version = "v1"
 
+// getPort from environment variable
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-// Will set up all of the developer-bot supplied endpoints.
-func Routes() *chi.Mux {
+	return port
+}
+
+// Serve the REST API over HTTP
+func Serve() {
+	port := getPort()
+	router := routes()
+
+	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(handler2 http.Handler) http.Handler) error {
+		log.Printf("%s %s\n", method, route)
+		return nil
+	}
+	if err := chi.Walk(router, walkFunc); err != nil {
+		log.Panicf("Logging error: %s\n", err.Error())
+	}
+
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
+
+// routes sets up routes
+func routes() *chi.Mux {
 	router := chi.NewRouter()
+
+	router.Use(
+		render.SetContentType(render.ContentTypeJSON),
+		middleware.Logger,
+		middleware.RedirectSlashes,
+		middleware.Recoverer,
+	)
 
 	router.Post("/endpoints", developer)
 
 	return router
 }
 
-
 func developer(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, http.StatusText(http.StatusNotImplemented), http.StatusNotImplemented)
 }
+
--- main.go
+++ main.go
@@ -2,44 +2,16 @@ package main
 
 import (
 	"developer-bot/endpoints"
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/render"
-	"log"
-	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	var port = os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	router := Routes()
+	go endpoints.Serve()
 
-	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(handler2 http.Handler) http.Handler) error {
-		log.Printf("%s %s\n", method, route)
-		return nil
-	}
-	if err := chi.Walk(router, walkFunc); err != nil {
-		log.Panicf("Logging error: %s\n", err.Error())
-	}
-
-	log.Fatal(http.ListenAndServe(":" + port, router))
-}
-
-func Routes() *chi.Mux {
-	router := chi.NewRouter()
-	router.Use(
-		render.SetContentType(render.ContentTypeJSON),
-		middleware.Logger,
-		middleware.RedirectSlashes,
-		middleware.Recoverer,
-	)
-
-	router.Route("/developer-bot", func(r chi.Router) {
-		r.Mount("/"+endpoints.Version, endpoints.Routes())
-	})
-
-	return router
+	// Wait for signal from the os before exiting
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-stop
 }
