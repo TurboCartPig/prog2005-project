181e8063afc596ceb5691828f271472620de4ff8
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-05-04 14:16:25 +0200

    Add discord command framework

--- discord/handlers/messageCreate.go
+++ discord/handlers/messageCreate.go
@@ -2,10 +2,13 @@ package handlers
 
 import (
 	"log"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"developer-bot/endpoints/firestore"
 )
+
+const HelpMessage = "Help!!!!"
 
 // MessageCreate handles messages being sent in any channel the bot has access to.
 func MessageCreate(s *discordgo.Session, msg *discordgo.MessageCreate) {
@@ -19,14 +22,36 @@ func MessageCreate(s *discordgo.Session, msg *discordgo.MessageCreate) {
 	for _, user := range msg.Mentions {
 		if user.ID == s.State.User.ID {
 			mentioned = true
+			break
+		}
+	}
+
+	// We only respond to messages were we are mentioned
+	if !mentioned {
+		return
+	}
+
+	if strings.HasPrefix(msg.Content, "!") {
+		command := msg.Content[1:]
+
+		if strings.HasPrefix(command, "help") {
+			// Print help message
+			_, err := s.ChannelMessageSend(msg.ChannelID, HelpMessage)
+			if err != nil {
+				log.Println("Failed to send message: ", err)
+			}
+		} else if strings.HasPrefix(command, "sub") {
+			// Subscribe to gitlab repo notifications
+		} else if strings.HasPrefix(command, "unsub") {
+			// Unsubscribe from repo
 		}
 	}
 
 	// If the bot was mentioned, send a message back
-	if mentioned {
-		_, err := s.ChannelMessageSend(msg.ChannelID, "I'm a very friendly and nice bot, I would neeeeveer insults you, uwu")
-		if err != nil {
-			log.Println("Failed to send message: ", err)
-		}
-	}
+	// if mentioned {
+	// 	_, err := s.ChannelMessageSend(msg.ChannelID, "I'm a very friendly and nice bot, I would neeeeveer insults you, uwu")
+	// 	if err != nil {
+	// 		log.Println("Failed to send message: ", err)
+	// 	}
+	// }
 }
